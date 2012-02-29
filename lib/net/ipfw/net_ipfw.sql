80e2788df1ec0ffbd93a686d4984267e63bbdf68
Tom Hornos <tom.hornos@gmail.com>
2012-02-29 23:23:24 +0100

    update

--- lib/net/ipfw/net_ipfw.sql
+++ lib/net/ipfw/net_ipfw.sql
@@ -1,18 +1,10 @@
-DROP TABLE IF EXISTS ipfw_rule;
-DROP TABLE IF EXISTS ipfw_tag;
+DROP TABLE IF EXISTS net_ipfw;
 
-CREATE TABLE ipfw_tag (
-  tag   TEXT NOT NULL,
-  ctime INTEGER NOT NULL,
-  valid INTEGER NOT NULL DEFAULT 0,
-  PRIMARY KEY(tag)
-);
-
-CREATE TABLE ipfw_rule (
-  id    NUMERIC NOT NULL,
+CREATE TABLE net_ipfw (
+  no    NUMERIC NOT NULL,
+  func  TEXT NOT NULL,
   rule  TEXT NOT NULL,
-  tag   TEXT NOT NULL,
-  PRIMARY KEY(id),
-  UNIQUE(id,rule),
-  FOREIGN KEY(tag) REFERENCES ipfw_tag(tag)
+  state NUMERIC NOT NULL DEFAULT 0,
+  PRIMARY KEY(no),
+  UNIQUE(no,func)
 );
