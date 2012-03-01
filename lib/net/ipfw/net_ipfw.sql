432e832ed34af466816e6a0429fc767834160460
Tom Hornos <tom.hornos@gmail.com>
2012-03-01 11:06:40 +0100

    update

--- lib/net/ipfw/net_ipfw.sql
+++ lib/net/ipfw/net_ipfw.sql
@@ -1,10 +1,11 @@
 DROP TABLE IF EXISTS net_ipfw;
 
 CREATE TABLE net_ipfw (
-  no    NUMERIC NOT NULL,
-  func  TEXT NOT NULL,
-  rule  TEXT NOT NULL,
-  state NUMERIC NOT NULL DEFAULT 0,
-  PRIMARY KEY(no),
-  UNIQUE(no,func)
+  -- rules
+  rno    NUMERIC NOT NULL,
+  rset   NUMERIC NOT NULL,
+  rfunc  TEXT NOT NULL,
+  rstate NUMERIC NOT NULL DEFAULT 0,
+  PRIMARY KEY(rno),
+  UNIQUE(rno,rfunc)
 );
